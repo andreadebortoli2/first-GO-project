24cf42738a66ac3eddca60dab4b5d6a3827dfc79
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-08-12 12:20:56 +0200

    add handlers.go and render.go to organize code

--- main.go
+++ main.go
@@ -3,30 +3,9 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"text/template"
 )
 
 const portNumber = ":8080"
-
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "home.page.tmpl.html")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplates(w, "about.page.tmpl.html")
-}
-
-func renderTemplates(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
-	}
-
-}
 
 // main is the main application function
 func main() {
