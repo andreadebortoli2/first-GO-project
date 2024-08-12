bc2c3961d74102185beff8fd2356e20c7ffdb011
andreadebortoli2 <debortoliandrea88@gmail.com>
2024-08-12 12:44:37 +0200

    edit directories structure to better organize the code

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/andreadebortoli2/first-GO-project/pkg/handlers"
 )
 
 const portNumber = ":8080"
@@ -10,8 +12,8 @@ const portNumber = ":8080"
 // main is the main application function
 func main() {
 
-	http.HandleFunc("/", Home)
-	http.HandleFunc("/about", About)
+	http.HandleFunc("/", handlers.Home)
+	http.HandleFunc("/about", handlers.About)
 
 	_, _ = fmt.Printf("Starting application on port %s", portNumber)
 	_ = http.ListenAndServe(portNumber, nil)
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,4 +1,4 @@
-package main
+package render
 
 import (
 	"fmt"
@@ -6,7 +6,8 @@ import (
 	"text/template"
 )
 
-func renderTemplates(w http.ResponseWriter, tmpl string) {
+// RenderTemplates renders templates using html/template
+func RenderTemplates(w http.ResponseWriter, tmpl string) {
 	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
 	err := parsedTemplate.Execute(w, nil)
 	if err != nil {
